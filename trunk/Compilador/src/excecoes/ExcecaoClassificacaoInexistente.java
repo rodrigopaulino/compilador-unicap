2b48629b321e0c0f8f5ffcd9ebc9a4883e4be796
rpfs08@gmail.com <rpfs08@gmail.com@f127bd19-a504-5552-8441-7f175c27d4d4>
2014-05-31 02:33:02 +0000

    Atualizacao

--- trunk/Compilador/src/excecoes/ExcecaoClassificacaoInexistente.java
+++ trunk/Compilador/src/excecoes/ExcecaoClassificacaoInexistente.java
@@ -1,5 +1,12 @@
+/*
+ * Este arquivo é propriedade de Rodrigo Paulino Ferreira de Souza.
+ * Nenhuma informação nele contida pode ser reproduzida,
+ * mostrada ou revelada sem permissão escrita do mesmo.
+ */
 package excecoes;
 
+/**
+ * 
+ */
 public class ExcecaoClassificacaoInexistente extends Exception {
-
 }
--- trunk/Compilador/src/util/Parser.java
+++ trunk/Compilador/src/util/Parser.java
@@ -1,5 +1,12 @@
+/*
+ * Este arquivo é propriedade de Rodrigo Paulino Ferreira de Souza.
+ * Nenhuma informação nele contida pode ser reproduzida,
+ * mostrada ou revelada sem permissão escrita do mesmo.
+ */
 package util;
 
+/**
+ * 
+ */
 public class Parser {
-
 }
