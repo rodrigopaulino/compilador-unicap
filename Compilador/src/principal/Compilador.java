9447ba5e6a444beba293644299fc757890b7bee7
rpfs08@gmail.com <rpfs08@gmail.com@f127bd19-a504-5552-8441-7f175c27d4d4>
2014-05-31 02:33:02 +0000

    Atualizacao

--- Compilador/src/principal/Compilador.java
+++ Compilador/src/principal/Compilador.java
@@ -1,31 +1,43 @@
+/*
+ * Este arquivo é propriedade de Rodrigo Paulino Ferreira de Souza.
+ * Nenhuma informação nele contida pode ser reproduzida,
+ * mostrada ou revelada sem permissão escrita do mesmo.
+ */
 package principal;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import util.Scanner;
+import util.Token;
 import excecoes.ExcecaoClassificacaoInexistente;
 import excecoes.ExcecaoCompilador;
 
-import util.Scanner;
-
+/**
+ * 
+ */
 public class Compilador {
+	//~ Metodos --------------------------------------------------------------------------------------------------------------------
 
-	/** 
+	/**
+	 * DOCUMENT ME!
+	 *
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		FileReader fReader;
 		BufferedReader buffReader;
-		
+		Token teste;
+
 		try {
-			fReader = new FileReader("teste.C");
+			fReader = new FileReader(args[0]);
 			buffReader = new BufferedReader(fReader);
-			
+
 			while (true) {
-				Scanner.getInstancia().executar(buffReader);
-				
-				if (Scanner.getInstancia().isFimArquivo()) {
+				teste = Scanner.getInstancia().executar(buffReader);
+
+				if (teste == null) {
 					break;
 				}
 			}
--- Compilador/src/util/Scanner.java
+++ Compilador/src/util/Scanner.java
@@ -1,3 +1,8 @@
+/*
+ * Este arquivo é propriedade de Rodrigo Paulino Ferreira de Souza.
+ * Nenhuma informação nele contida pode ser reproduzida,
+ * mostrada ou revelada sem permissão escrita do mesmo.
+ */
 package util;
 
 import java.io.BufferedReader;
@@ -7,19 +12,28 @@ import java.util.HashMap;
 import excecoes.ExcecaoClassificacaoInexistente;
 import excecoes.ExcecaoCompilador;
 
+/**
+ * 
+ */
 public final class Scanner {
+	//~ Atributos/inicializadores estaticos ----------------------------------------------------------------------------------------
+
 	private static Scanner aInstancia;
-	private int aColuna = 0;
-	private int aLinha = 1;
-	private Token aUltimoTokenLido;
-	private Character aLookAheadAnterior;
-	private Character aLookAhead;
-	private boolean aIsComentarioLinha = false;
-	private boolean aIsComentarioBloco = false;
-	private boolean aInFimArquivo = false;
-	
 	private static HashMap<String, Short> aPalavrasReservadas;
-	
+
+	//~ Atributos de instancia -----------------------------------------------------------------------------------------------------
+
+	private Character aLookAhead;
+	private Token aUltimoTokenLido;
+	private boolean aInFimArquivo = false;
+	private int aColuna = 0;
+	private int aLinha = 0;
+
+	//~ Construtores ---------------------------------------------------------------------------------------------------------------
+
+/**
+         * Cria um novo objeto Scanner.
+         */
 	private Scanner() {
 		Scanner.aPalavrasReservadas = new HashMap<String, Short>();
 		Scanner.aPalavrasReservadas.put("main", Classificacao.MAIN);
@@ -32,265 +46,313 @@ public final class Scanner {
 		Scanner.aPalavrasReservadas.put("float", Classificacao.FLOAT);
 		Scanner.aPalavrasReservadas.put("char", Classificacao.CHAR);
 	}
-	
+
+	//~ Metodos --------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * -
+	 *
+	 * @return
+	 */
 	public static Scanner getInstancia() {
 		if (aInstancia == null) {
 			aInstancia = new Scanner();
 		}
-		
+
 		return aInstancia;
 	}
-	
-	public Token executar(BufferedReader pBuffReader) throws IOException, ExcecaoClassificacaoInexistente, ExcecaoCompilador{
+
+	/**
+	 * -
+	 *
+	 * @param pBuffReader
+	 *
+	 * @return
+	 *
+	 * @throws IOException
+	 * @throws ExcecaoClassificacaoInexistente
+	 * @throws ExcecaoCompilador
+	 */
+	public Token executar(BufferedReader pBuffReader) throws IOException, ExcecaoClassificacaoInexistente, ExcecaoCompilador {
 		String lexema = "";
-		
+
 		while (!this.aInFimArquivo) {
+			while ((this.aLookAhead == null) || Character.isWhitespace(this.aLookAhead)) {
+				this.lookAhead(pBuffReader);
+			}
+
 			switch (this.aLookAhead) {
-			case '\n':
-				break;
-			case '\t':
-				break;
-			case ' ':
-				break;
-			case '+':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco)
-					return this.aUltimoTokenLido = new Token(Classificacao.SOMA, "+");
-				
-				break;
-			case '-':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco)
-					return this.aUltimoTokenLido = new Token(Classificacao.SUBTRACAO, "-");
-				
-				break;
-			case '*':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco)
-					return this.aUltimoTokenLido = new Token(Classificacao.MULTIPLICACAO, "*");
-				
-				break;
-			case '(':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco)
-					return this.aUltimoTokenLido = new Token(Classificacao.PARENTESES_ABRE, "(");
-				
-				break;
-			case ')':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco)
-					return this.aUltimoTokenLido = new Token(Classificacao.PARENTESES_FECHA, ")");
-				
-				break;
-			case '{':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco)
-					return this.aUltimoTokenLido = new Token(Classificacao.CHAVE_ABRE, "{");
-				
-				break;
-			case '}':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco)
-					return this.aUltimoTokenLido = new Token(Classificacao.CHAVE_FECHA, "}");
-				
-				break;
-			case ',':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco)
-					return this.aUltimoTokenLido = new Token(Classificacao.VIRGULA, ",");
-				
-				break;
-			case ';':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco)
-					return this.aUltimoTokenLido = new Token(Classificacao.PONTO_VIRGULA, ";");
-				
-				break;
-			case '<':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco) {
+				case '+':
 					this.lookAhead(pBuffReader);
-					
-					if (this.aLookAhead == '=')
-						return this.aUltimoTokenLido = new Token(Classificacao.MENOR_IGUAL, "<=");
-					else
-						return this.aUltimoTokenLido = new Token(Classificacao.MENOR, "<");
-				}
-				break;
-			case '>':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco) {
+
+					return this.aUltimoTokenLido = new Token(Classificacao.SOMA);
+
+				case '-':
 					this.lookAhead(pBuffReader);
-					
-					if (this.aLookAhead == '=')
-						return this.aUltimoTokenLido = new Token(Classificacao.MAIOR_IGUAL, ">=");
-					else
-						return this.aUltimoTokenLido = new Token(Classificacao.MAIOR, ">");
-				}
-				break;
-			case '=':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco) {
+
+					return this.aUltimoTokenLido = new Token(Classificacao.SUBTRACAO);
+
+				case '*':
 					this.lookAhead(pBuffReader);
-					
-					if (this.aLookAhead == '=')
-						return this.aUltimoTokenLido = new Token(Classificacao.IGUAL, "==");
-					else
-						return this.aUltimoTokenLido = new Token(Classificacao.ATRIBUICAO, "=");
-				}
-				break;
-			case '!':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco) {
+
+					return this.aUltimoTokenLido = new Token(Classificacao.MULTIPLICACAO);
+
+				case '(':
 					this.lookAhead(pBuffReader);
-					
-					if (this.aLookAhead == '=')
-						return this.aUltimoTokenLido = new Token(Classificacao.DIFERENTE, "!");
-					else
-						throw new ExcecaoCompilador(this.aLinha, this.aColuna, (this.aUltimoTokenLido != null)?this.aUltimoTokenLido.getLexema():"", "Operador Relacional Invalido.");
-				}
-				break;
-			case '/':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco) {
+
+					return this.aUltimoTokenLido = new Token(Classificacao.PARENTESES_ABRE);
+
+				case ')':
 					this.lookAhead(pBuffReader);
-					
-					if (this.aLookAhead == '/')
-						this.aIsComentarioLinha = true;
-					else if (this.aLookAhead == '*')
-						this.aIsComentarioBloco = true;
-					else
-						return this.aUltimoTokenLido = new Token(Classificacao.DIVISAO, "/");
-				}
-				break;
-			case '\'':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco) {
+
+					return this.aUltimoTokenLido = new Token(Classificacao.PARENTESES_FECHA);
+
+				case '{':
+					this.lookAhead(pBuffReader);
+
+					return this.aUltimoTokenLido = new Token(Classificacao.CHAVE_ABRE);
+
+				case '}':
+					this.lookAhead(pBuffReader);
+
+					return this.aUltimoTokenLido = new Token(Classificacao.CHAVE_FECHA);
+
+				case ',':
+					this.lookAhead(pBuffReader);
+
+					return this.aUltimoTokenLido = new Token(Classificacao.VIRGULA);
+
+				case ';':
+					this.lookAhead(pBuffReader);
+
+					return this.aUltimoTokenLido = new Token(Classificacao.PONTO_VIRGULA);
+
+				case '!':
+					this.lookAhead(pBuffReader);
+
+					if (this.aLookAhead == '=') {
+						this.lookAhead(pBuffReader);
+
+						return this.aUltimoTokenLido = new Token(Classificacao.DIFERENTE);
+					} else {
+						throw new ExcecaoCompilador(this.aLinha, this.aColuna,
+							(this.aUltimoTokenLido != null) ? this.aUltimoTokenLido.getClassificacao().getDescricao() : "",
+							"Operador Relacional Invalido.");
+					}
+
+				case '<':
+					this.lookAhead(pBuffReader);
+
+					if (this.aLookAhead == '=') {
+						this.lookAhead(pBuffReader);
+
+						return this.aUltimoTokenLido = new Token(Classificacao.MENOR_IGUAL);
+					} else {
+						return this.aUltimoTokenLido = new Token(Classificacao.MENOR);
+					}
+
+				case '>':
+					this.lookAhead(pBuffReader);
+
+					if (this.aLookAhead == '=') {
+						this.lookAhead(pBuffReader);
+
+						return this.aUltimoTokenLido = new Token(Classificacao.MAIOR_IGUAL);
+					} else {
+						return this.aUltimoTokenLido = new Token(Classificacao.MAIOR);
+					}
+
+				case '=':
+					this.lookAhead(pBuffReader);
+
+					if (this.aLookAhead == '=') {
+						this.lookAhead(pBuffReader);
+
+						return this.aUltimoTokenLido = new Token(Classificacao.IGUAL);
+					} else {
+						return this.aUltimoTokenLido = new Token(Classificacao.ATRIBUICAO);
+					}
+
+				case '\'':
 					lexema += this.aLookAhead.toString();
-					
 					this.lookAhead(pBuffReader);
-					
+
 					if (Character.isDigit(this.aLookAhead) || Character.isLetter(this.aLookAhead)) {
 						lexema += this.aLookAhead.toString();
-						
+
 						this.lookAhead(pBuffReader);
-						
+
 						if (this.aLookAhead == '\'') {
 							lexema += this.aLookAhead.toString();
-							
+
+							this.lookAhead(pBuffReader);
+
 							return this.aUltimoTokenLido = new Token(Classificacao.CARACTER, lexema);
 						} else {
-							throw new ExcecaoCompilador(this.aLinha, this.aColuna, (this.aUltimoTokenLido != null)?this.aUltimoTokenLido.getLexema():"", "Caracter Invalido.");
+							throw new ExcecaoCompilador(this.aLinha, this.aColuna,
+								(this.aUltimoTokenLido != null) ? this.aUltimoTokenLido.getClassificacao().getDescricao() : "",
+								"Caracter Invalido.");
 						}
 					} else {
-						throw new ExcecaoCompilador(this.aLinha, this.aColuna, (this.aUltimoTokenLido != null)?this.aUltimoTokenLido.getLexema():"", "Caracter Invalido.");
+						throw new ExcecaoCompilador(this.aLinha, this.aColuna,
+							(this.aUltimoTokenLido != null) ? this.aUltimoTokenLido.getClassificacao().getDescricao() : "",
+							"Token do tipo Caracter Invalido.");
 					}
-				}
-				break;
-			case '.':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco) {
+
+				case '.':
 					lexema += this.aLookAhead.toString();
-					
 					this.lookAhead(pBuffReader);
-					
-					while(Character.isDigit(this.aLookAhead)){
+
+					while (Character.isDigit(this.aLookAhead)) {
 						lexema += this.aLookAhead.toString();
-						
+
 						this.lookAhead(pBuffReader);
 					}
-					
+
 					if (lexema.charAt(lexema.length() - 1) == '.') {
-						throw new ExcecaoCompilador(this.aLinha, this.aColuna, (this.aUltimoTokenLido != null)?this.aUltimoTokenLido.getLexema():"", "Numero Decimal Invalido.");
+						throw new ExcecaoCompilador(this.aLinha, this.aColuna,
+							(this.aUltimoTokenLido != null) ? this.aUltimoTokenLido.getClassificacao().getDescricao() : "",
+							"Numero Decimal Invalido.");
 					} else {
 						return this.aUltimoTokenLido = new Token(Classificacao.REAL, lexema);
 					}
-				}
-				break;
-			case '_':
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco) {
+
+				case '_':
 					lexema += this.aLookAhead.toString();
-					
-					this.aColuna++;
 					this.lookAhead(pBuffReader);
-					
-					while (Character.isDigit(this.aLookAhead) || Character.isLetter(this.aLookAhead) || this.aLookAhead == '_') {
+
+					while (Character.isDigit(this.aLookAhead) || Character.isLetter(this.aLookAhead) || (this.aLookAhead == '_')) {
 						lexema += this.aLookAhead.toString();
-						
-						this.aColuna++;
+
 						this.lookAhead(pBuffReader);
 					}
-					
+
 					return this.aUltimoTokenLido = new Token(Classificacao.ID, lexema);
-				}
-				break;
-			default:
-				if (this.isFimArquivo())
-					break;
-				if (!this.aIsComentarioLinha && !this.aIsComentarioBloco) {
-					lexema += this.aLookAhead.toString();
-					
-					if (Character.isLetter(this.aLookAhead)) {
-						this.aColuna++;
+
+				case '/':
+					this.lookAhead(pBuffReader);
+
+					if (this.aLookAhead == '/') {
 						this.lookAhead(pBuffReader);
-						
-						while(Character.isLetter(this.aLookAhead) || Character.isDigit(this.aLookAhead) || this.aLookAhead == '_'){
-							lexema += this.aLookAhead.toString();
-							
-							this.aColuna++;
+
+						while (this.aLookAhead != '\n') {
 							this.lookAhead(pBuffReader);
 						}
-						
-						if (Scanner.aPalavrasReservadas.containsKey(lexema) && (this.aLookAhead == ' ' || this.aLookAhead == '+'
-								|| this.aLookAhead == '-' || this.aLookAhead == '*' || this.aLookAhead == '/'
-								|| this.aLookAhead == '=' || this.aLookAhead == ')' || this.aLookAhead == '('
-								|| this.aLookAhead == '{' || this.aLookAhead == '}' || this.aLookAhead == ','
-								|| this.aLookAhead == ';')) {
+					} else if (this.aLookAhead == '*') {
+						while (true) {
+							this.lookAhead(pBuffReader);
+
+							if (this.aInFimArquivo) {
+								throw new ExcecaoCompilador(this.aLinha, this.aColuna,
+									(this.aUltimoTokenLido != null) ? this.aUltimoTokenLido.getClassificacao().getDescricao() : "",
+									"Fim de arquivo antes de fim de comentario.");
+							}
+
+							if (this.aLookAhead == '*') {
+								this.lookAhead(pBuffReader);
+
+								while (this.aLookAhead == '*') {
+									this.lookAhead(pBuffReader);
+								}
+
+								if (this.aInFimArquivo) {
+									throw new ExcecaoCompilador(this.aLinha, this.aColuna,
+										(this.aUltimoTokenLido != null) ? this.aUltimoTokenLido.getClassificacao().getDescricao() : "",
+										"Fim de arquivo antes de fim de comentario.");
+								}
+
+								if (this.aLookAhead == '/') {
+									this.lookAhead(pBuffReader);
+
+									break;
+								}
+							}
+						}
+					} else {
+						return this.aUltimoTokenLido = new Token(Classificacao.DIVISAO);
+					}
+
+					break;
+
+				default:
+
+					if (this.aInFimArquivo) {
+						break;
+					}
+					lexema += this.aLookAhead.toString();
+
+					if (Character.isLetter(this.aLookAhead)) {
+						this.lookAhead(pBuffReader);
+
+						while (Character.isLetter(this.aLookAhead) || Character.isDigit(this.aLookAhead) ||
+								(this.aLookAhead == '_')) {
+							lexema += this.aLookAhead.toString();
+
+							this.lookAhead(pBuffReader);
+						}
+
+						if (Scanner.aPalavrasReservadas.containsKey(lexema)) {
 							return this.aUltimoTokenLido = new Token(Scanner.aPalavrasReservadas.get(lexema), lexema);
-						} else if (Scanner.aPalavrasReservadas.containsKey(lexema)) {
-							throw new ExcecaoCompilador(this.aLinha, this.aColuna, (this.aUltimoTokenLido != null)?this.aUltimoTokenLido.getLexema():"", "Palavra Reservada Nao Delimitada Corretamente.");
 						} else {
 							return this.aUltimoTokenLido = new Token(Classificacao.ID, lexema);
 						}
 					} else if (Character.isDigit(this.aLookAhead)) {
-						this.aColuna++;
 						this.lookAhead(pBuffReader);
-						
-						while(Character.isDigit(this.aLookAhead)){
+
+						while (Character.isDigit(this.aLookAhead)) {
 							lexema += this.aLookAhead.toString();
-							
-							this.aColuna++;
+
 							this.lookAhead(pBuffReader);
 						}
-						
+
 						if (this.aLookAhead != '.') {
 							return this.aUltimoTokenLido = new Token(Classificacao.INTEIRO, lexema);
 						}
 					} else {
-						throw new ExcecaoCompilador(this.aLinha, this.aColuna, (this.aUltimoTokenLido != null)?this.aUltimoTokenLido.getLexema():"", "Caracter Nao Reconhecido.");
+						throw new ExcecaoCompilador(this.aLinha, this.aColuna,
+							(this.aUltimoTokenLido != null) ? this.aUltimoTokenLido.getClassificacao().getDescricao() : "",
+							"Caracter Nao Reconhecido.");
 					}
-				}
-				break;
 			}
 		}
+
 		return null;
 	}
-	
-	private void lookAhead(BufferedReader pBuffReader) throws IOException, ExcecaoCompilador {
+
+	/**
+	 * -
+	 *
+	 * @param pBuffReader
+	 *
+	 * @throws IOException
+	 * @throws ExcecaoCompilador
+	 */
+	private void lookAhead(BufferedReader pBuffReader)
+		throws IOException, ExcecaoCompilador {
 		int intChar = pBuffReader.read();
-		
+
 		if (intChar == -1) {
 			this.aInFimArquivo = true;
 		} else if (intChar == 13) {
 			intChar = pBuffReader.read();
-			
+
 			if (intChar != 10) {
-				throw new ExcecaoCompilador(this.aLinha, this.aColuna
-						, (this.aUltimoTokenLido != null)?this.aUltimoTokenLido.getLexema():""
-							, "CR-LF Invalido.");
+				throw new ExcecaoCompilador(this.aLinha, this.aColuna,
+					(this.aUltimoTokenLido != null) ? this.aUltimoTokenLido.getClassificacao().getDescricao() : "",
+					"CR-LF Invalido.");
 			}
-			
+
 			this.aColuna = 0;
 			this.aLinha++;
 		} else if (intChar == 10) {
-			throw new ExcecaoCompilador(this.aLinha, this.aColuna
-					, (this.aUltimoTokenLido != null)?this.aUltimoTokenLido.getLexema():""
-						, "CR-LF Invalido.");
+			throw new ExcecaoCompilador(this.aLinha, this.aColuna,
+				(this.aUltimoTokenLido != null) ? this.aUltimoTokenLido.getClassificacao().getDescricao() : "", "CR-LF Invalido.");
 		} else if (intChar == 9) {
 			this.aColuna = this.aColuna + 4;
 		} else {
 			this.aColuna++;
 		}
-		
-		this.aLookAheadAnterior = this.aLookAhead;
+
 		this.aLookAhead = (char) intChar;
-	}
-	
-	public boolean isFimArquivo() {
-		return this.aInFimArquivo;
 	}
 }
