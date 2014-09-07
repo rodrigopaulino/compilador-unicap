88d710b80a3d43c35aa5d707a6c6fd6f8bf75a20
rpfs08@gmail.com <rpfs08@gmail.com@f127bd19-a504-5552-8441-7f175c27d4d4>
2014-09-07 18:10:07 +0000

    Atualizacao

---
+++ Compilador/src/ExcecaoCompilador.java
@@ -0,0 +1,46 @@
+/*
+ * Este arquivo é propriedade de Rodrigo Paulino Ferreira de Souza.
+ * Nenhuma informação nele contida pode ser reproduzida,
+ * mostrada ou revelada sem permissão escrita do mesmo.
+ */
+
+
+/**
+ * 
+ */
+public class ExcecaoCompilador extends Exception {
+	//~ Atributos/inicializadores estaticos ----------------------------------------------------------------------------------------
+
+	private static final long serialVersionUID = 2392227523068350450L;
+
+	//~ Atributos de instancia -----------------------------------------------------------------------------------------------------
+
+	private String aMessagem;
+
+	//~ Construtores ---------------------------------------------------------------------------------------------------------------
+
+/**
+         * Cria um novo objeto ExcecaoCompilador.
+         *
+         * @param pLinha  
+         * @param pColuna  
+         * @param pUltimoToken  
+         * @param pErro  
+         */
+	public ExcecaoCompilador(int pLinha, int pColuna, String pUltimoToken, String pErro) {
+		super();
+		this.aMessagem = "ERRO na linha " + pLinha + ", coluna " + pColuna + ", ultimo token lido \"" + pUltimoToken + "\":" +
+			pErro;
+	}
+
+	//~ Metodos --------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * -
+	 *
+	 * @return
+	 */
+	public String getMessage() {
+		return this.aMessagem;
+	}
+}
---
+++ Compilador/src/Token.java
@@ -0,0 +1,62 @@
+/*
+ * Este arquivo é propriedade de Rodrigo Paulino Ferreira de Souza.
+ * Nenhuma informação nele contida pode ser reproduzida,
+ * mostrada ou revelada sem permissão escrita do mesmo.
+ */
+
+
+/**
+ * 
+ */
+public class Token {
+	//~ Atributos de instancia -----------------------------------------------------------------------------------------------------
+
+	private Classificacao aClassificacao;
+	private String aLexema;
+
+	//~ Construtores ---------------------------------------------------------------------------------------------------------------
+
+/**
+         * Cria um novo objeto Token.
+         *
+         * @param pClassificacao  
+         * @param pLexema  
+         *
+         * @throws ExcecaoClassificacaoInexistente  
+         */
+	public Token(short pClassificacao, String pLexema) {
+		this.aClassificacao = new Classificacao(pClassificacao);
+		this.aLexema = pLexema;
+	}
+
+/**
+         * Cria um novo objeto Token.
+         *
+         * @param pClassificacao  
+         *
+         * @throws ExcecaoClassificacaoInexistente  
+         */
+	public Token(short pClassificacao) {
+		this.aClassificacao = new Classificacao(pClassificacao);
+	}
+
+	//~ Metodos --------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * -
+	 *
+	 * @return
+	 */
+	public Classificacao getClassificacao() {
+		return this.aClassificacao;
+	}
+
+	/**
+	 * -
+	 *
+	 * @return
+	 */
+	public String getLexema() {
+		return this.aLexema;
+	}
+}
